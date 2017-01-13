adc1163c11c1b8f318e670dea5fe267cba44ff2e
Jordan Hall <jordan.hall@rapidweb.biz>
2017-01-13 15:04:37 +0000

    Test file connection and data output

--- test/index.php
+++ test/index.php
@@ -5,3 +5,9 @@ require 'vendor/autoload.php';
 use \DivineOmega\DCOM\DCOM;
 
 $mysqli = DCOM::getConnection("main");
+
+$result = $mysqli->query('select * from media limit 1');
+
+$row = $result->fetch_assoc();
+
+var_dump($row);
