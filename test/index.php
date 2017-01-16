3850dc398c93cf1174257fc335e6a06f0671a706
Jordan Hall <jordan.hall@rapidweb.biz>
2017-01-16 11:08:28 +0000

    PDO support

--- test/index.php
+++ test/index.php
@@ -4,10 +4,17 @@ require 'vendor/autoload.php';
 
 use \DivineOmega\DCOM\DCOM;
 
+// mysqli
+
 $mysqli = DCOM::getConnection("main");
-
 $result = $mysqli->query('select * from media limit 1');
+while ($row = $result->fetch_assoc()) {
+    var_dump($row);
+}
 
-$row = $result->fetch_assoc();
+// PDO
 
-var_dump($row);
+$pdo = DCOM::getConnection("main");
+foreach ($pdo->query('select * from media limit 1') as $row) {
+    var_dump($row);
+}
